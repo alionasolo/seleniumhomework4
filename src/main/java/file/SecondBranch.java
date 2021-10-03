41dcd58ba37b83f4eadb39f52ecfa171cbb932f1
alionasolo <ali.solonari@mail.ru>
2021-10-03 23:57:26 +0300

    add second class v2.2

--- src/main/java/file/SecondBranch.java
+++ src/main/java/file/SecondBranch.java
@@ -1,7 +1,7 @@
 package file;
 
-public class SecondClass {
+public class SecondBranch {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("hello!");
     }
 }
